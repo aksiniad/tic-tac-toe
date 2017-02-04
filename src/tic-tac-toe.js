599091fce5fee76e207bfcf875e0a5e6b775e484
Aksinnia D <smert.na.diete@gmail.com>
2017-02-05 02:18:53 +0300

    1st test passed

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,28 @@
 class TicTacToe {
     constructor() {
-
+        this.markPlayerX = 'x';
+        this.markPlayerO = 'o';
+        this.turn = 0;
+        this.board =new Array (
+      new Array ('','',''),
+      new Array ('','',''),
+      new Array ('','',''));
+        this.isDraw = false;
+        
     }
 
     getCurrentPlayerSymbol() {
-
+        if (this.turn%2 != 1){
+            this.CurrentPlayerSymbol =  this.markPlayerX; 
+        }else{
+            this.CurrentPlayerSymbol =  this.markPlayerO;
+        }
+        return this.CurrentPlayerSymbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.board[rowIndex][columnIndex] === '') {this.board[rowIndex][columnIndex] = this.CurrentPlayerSymbol;
+        this.turn+=1;} 
     }
 
     isFinished() {
@@ -16,6 +30,7 @@ class TicTacToe {
     }
 
     getWinner() {
+
 
     }
 
@@ -31,5 +46,5 @@ class TicTacToe {
 
     }
 }
-
+game = new TicTacToe();
 module.exports = TicTacToe;
